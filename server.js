ef4f13f0a45a95b2ead4141c80af65e35b34483b
Arosilho99 <anderson_rossilho@live.com>
2024-12-28 10:17:30 -0300

    implantando endpoint login

--- server.js
+++ server.js
@@ -1,10 +1,22 @@
 const express = require('express');
 const cors = require('cors');
 const axios = require('axios');
-require('dotenv').config();
-
+require('dotenv').config(); // Carregar variáveis de ambiente
+const { Pool } = require('pg');
 const app = express();
 const port = process.env.PORT || 3000;
+
+// Configuração do Pool de Conexão com o PostgreSQL
+const pool = new Pool({
+  user: process.env.PG_USER, // Use variável de ambiente para segurança
+  host: process.env.PG_HOST,
+  database: process.env.PG_DATABASE,
+  password: process.env.PG_PASSWORD,
+  port: process.env.PG_PORT || 5432,
+  ssl: {
+    rejectUnauthorized: false, // Pode ser necessário para conexões SSL em bancos de dados na nuvem
+  },
+});
 
 // Configuração CORS para permitir todas as origens
 const corsOptions = {
@@ -13,11 +25,8 @@ const corsOptions = {
   allowedHeaders: ['Content-Type', 'Authorization'],
 };
 
-// Aplica o middleware CORS com a configuração
-app.use(cors(corsOptions));
-
-// Middleware para parsing JSON
-app.use(express.json());
+app.use(cors(corsOptions)); // Aplica o middleware CORS
+app.use(express.json()); // Middleware para parsing de JSON
 
 // Endpoint para enviar PIX
 app.post('/send-pix', async (req, res) => {
@@ -36,7 +45,7 @@ app.post('/send-pix', async (req, res) => {
     headers: {
       accept: 'application/json',
       'content-type': 'application/json',
-      access_token: process.env.ASAAS_ACCESS_TOKEN || 'SEU_ACCESS_TOKEN',
+      access_token: process.env.ASAAS_ACCESS_TOKEN, // Use o token da variável de ambiente
     },
     data,
   };
@@ -52,48 +61,68 @@ app.post('/send-pix', async (req, res) => {
 
 // Endpoint para receber PIX
 app.post('/receive-pix', async (req, res) => {
-    const { value, description } = req.body;
-  
-    if (!description || !value) {
-      return res.status(400).json({ error: 'Empty Pix value and description' });
-    }
-  
-    const url = 'https://www.asaas.com/api/v3/paymentLinks';
-    const options = {
-      method: 'POST',
-      headers: {
-        accept: 'application/json',
-        'content-type': 'application/json',
-        access_token: process.env.ASAAS_ACCESS_TOKEN || 'SEU_ACCESS_TOKEN',
+  const { value, description } = req.body;
+
+  if (!description || !value) {
+    return res.status(400).json({ error: 'Empty Pix value and description' });
+  }
+
+  const url = 'https://www.asaas.com/api/v3/paymentLinks';
+  const data = {
+    billingType: 'UNDEFINED',
+    chargeType: 'DETACHED',
+    name: 'Convite Perfeito',
+    value,
+    dueDateLimitDays: 2,
+    description,
+  };
+
+  const options = {
+    method: 'POST',
+    headers: {
+      accept: 'application/json',
+      'content-type': 'application/json',
+      access_token: process.env.ASAAS_ACCESS_TOKEN, // Use o token da variável de ambiente
     },
-      body: JSON.stringify({
-        billingType: 'UNDEFINED',
-        chargeType: 'DETACHED',
-        name: 'Convite Perfeito',
-        value,
-        dueDateLimitDays: 2,
-        description,
-      }),
-    };
-  
-    try {
-      const response = await fetch(url, options);
-      const data = await response.json();
-  
-      // Verifica se a API retornou o campo `url`
-      if (data && data.url) {
-        return res.status(200).json({ url: data.url });
-      } else {
-        return res.status(500).json({ error: 'Failed to retrieve payment URL' });
-      }
-    } catch (error) {
-      console.error('Error processing Pix payment link:', error.message);
-      return res.status(500).json({ error: 'Internal Server Error' });
+    data,
+  };
+
+  try {
+    const response = await axios.request(options);
+    if (response.data && response.data.url) {
+      return res.status(200).json({ url: response.data.url });
+    } else {
+      return res.status(500).json({ error: 'Failed to retrieve payment URL' });
     }
-  });
-  
-  // Iniciar o servidor
-  app.listen(port, '0.0.0.0', () => {
-    console.log(`Server running on port ${port}`);
-  });
-  
+  } catch (error) {
+    console.error('Error processing Pix payment link:', error.message);
+    return res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+// Endpoint para login
+app.post('/login', async (req, res) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return res.status(400).json({ success: false, message: 'Email and password are required' });
+  }
+
+  try {
+    const result = await pool.query('SELECT * FROM users WHERE email = $1 AND password = $2', [email, password]);
+
+    if (result.rows.length > 0) {
+      res.json({ success: true, message: 'Login successful' });
+    } else {
+      res.json({ success: false, message: 'Invalid credentials' });
+    }
+  } catch (error) {
+    console.error('Error logging in:', error);
+    res.status(500).json({ success: false, message: 'Server error', error });
+  }
+});
+
+// Iniciar o servidor
+app.listen(port, '0.0.0.0', () => {
+  console.log(`Server running on port ${port}`);
+});
